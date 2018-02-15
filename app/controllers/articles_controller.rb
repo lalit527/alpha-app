11780103da5954a3d99f72ff87db2b9bed3f5827
lalit527 <lalit.slg007@gmail.com>
2018-02-15 15:05:31 +0000

    Made controller changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   
   def index
     @articles = Article.paginate(page: params[:page], per_page:5)  
@@ -21,15 +22,15 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+    
   end
   
   def edit
-    @article = Article.find_by_id(params[:id])
+    
   end
   
   def update
-    @article = Article.find(params[:id])
+    
     if @article.update(article_params)
       flash[:success] = "Article updated"
       redirect_to article_path(@article)
@@ -39,7 +40,6 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:success] = "Article deleted"
     redirect_to articles_path
@@ -48,5 +48,9 @@ class ArticlesController < ApplicationController
   private
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+    
+    def set_article
+      @article = Article.find(params[:id])
     end
 end
